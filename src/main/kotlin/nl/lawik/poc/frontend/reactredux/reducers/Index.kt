ddb880a92be3e7e3eea97f3b9676cd7c0162af5c
Viktor.Noskin <viktor.noskin@jetbrains.com>
2021-07-31 18:12:36 +0300

    refactoring: remove unused combined reducer

--- src/main/kotlin/nl/lawik/poc/frontend/reactredux/reducers/Index.kt
+++ src/main/kotlin/nl/lawik/poc/frontend/reactredux/reducers/Index.kt
@@ -2,20 +2,11 @@ package nl.lawik.poc.frontend.reactredux.reducers
 
 import nl.lawik.poc.frontend.reactredux.entities.Todo
 import nl.lawik.poc.frontend.reactredux.enums.VisibilityFilter
-import nl.lawik.poc.frontend.reactredux.util.combineReducers
 import redux.RAction
-
 
 data class State(
     val todos: Array<Todo> = emptyArray(),
     val visibilityFilter: VisibilityFilter = VisibilityFilter.SHOW_ALL
-)
-
-fun combinedReducers() = combineReducers(
-    mapOf(
-        State::todos to ::todos,
-        State::visibilityFilter to ::visibilityFilter
-    )
 )
 
 fun rootReducer(
